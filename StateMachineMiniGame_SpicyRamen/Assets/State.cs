de4b88bf12cef29350e8bf49d096b7f8e894efc2
pamelanelson1215 <54724859+pamelanelson1215@users.noreply.github.com>
2020-09-07 11:48:29 -0600

    Implemented State Machine and added player

---
+++ StateMachineMiniGame_SpicyRamen/Assets/State.cs
@@ -0,0 +1,23 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class State
+{
+    protected StateController stateController;
+
+    public abstract void CheckTransitions();
+
+    public abstract void Act();
+
+    public virtual void OnStateEnter() { }
+
+    public virtual void OnStateExit() { }
+
+   //constructor
+   public State(StateController stateController)
+    {
+        this.stateController = stateController;
+    }
+}
